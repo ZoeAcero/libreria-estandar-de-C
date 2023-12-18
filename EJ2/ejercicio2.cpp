a103b85dcdc9eb9688301284384a11a7d45b4998
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:54:02 +0100

    3

--- EJ2/ejercicio2.cpp
+++ EJ2/ejercicio2.cpp
@@ -16,4 +16,12 @@ int main() {
         printf("Las cadenas son diferentes.\n");
     }
 
+    size_t longitud_cadena = strlen(cadena1);
+
+    // Muestra la longitud de la cadena
+    printf("La longitud de la cadena \"%s\" es %zu caracteres.\n", cadena1, longitud_cadena);
+
+    return 0;
+}
+
 
