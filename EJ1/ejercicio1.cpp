b10507bbda239c8d80682f7f73e77d06b1d21421
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:45:29 +0100

    1

---
+++ EJ1/ejercicio1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    
+    int numero_entero = -5;
+    int valor_absoluto = abs(numero_entero);
+    printf("El valor absoluto de %d es %d\n", numero_entero, valor_absoluto);
