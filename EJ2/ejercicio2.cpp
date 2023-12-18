3a26a6149c1fa694eeda24ab12031ed95682bc02
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:52:58 +0100

    1

---
+++ EJ2/ejercicio2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+int main() {
+    // Crea dos cadenas de caracteres
+    char cadena1[] = "Hola, mundo!";
+    char cadena2[] = "Hola, mundo!";
+
+
