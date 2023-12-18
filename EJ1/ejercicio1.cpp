b3fe96ea0396f365776a2ce027d0645328387618
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:46:13 +0100

    2

--- EJ1/ejercicio1.cpp
+++ EJ1/ejercicio1.cpp
@@ -2,7 +2,13 @@
 #include <cstring>
 
 int main() {
-    
+
     int numero_entero = -5;
     int valor_absoluto = abs(numero_entero);
     printf("El valor absoluto de %d es %d\n", numero_entero, valor_absoluto);
+
+    srand(time(NULL));  // Inicializa la semilla con el tiempo actual
+    int numero_aleatorio = rand() % 100 + 1;
+    printf("Número aleatorio: %d\n", numero_aleatorio);
+
+    
