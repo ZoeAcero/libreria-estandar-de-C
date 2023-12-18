946c1119fd942dbfe65a5902ffff91b8147352bf
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:57:33 +0100

    2

--- EJ3/ejercicio3.cpp
+++ EJ3/ejercicio3.cpp
@@ -4,10 +4,15 @@
 
 int main() {
 
-}
+
     const char* nombre_archivo = "ejemplo.txt";
 
     // Intenta abrir el archivo en modo lectura
     FILE* archivo = fopen(nombre_archivo, "r");
 
-    
+if (archivo == NULL) {
+
+printf("Error: No se pudo abrir el archivo %s.\n", nombre_archivo);
+} else {
+
+
