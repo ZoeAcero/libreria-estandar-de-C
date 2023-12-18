d5216421d0d1b38e6a2779d0dfc0eee08d4c9e91
ZoeAcero <zar.amazona@gmail.com>
2023-12-18 12:56:28 +0100

    1

---
+++ EJ3/ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+int main() {
+
+}
+    const char* nombre_archivo = "ejemplo.txt";
+
+    // Intenta abrir el archivo en modo lectura
+    FILE* archivo = fopen(nombre_archivo, "r");
+
+    
